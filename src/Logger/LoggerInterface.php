1c08d186ff933ac5719b02240b642b05be47d06b
Gaurav Gupta <gaurav.gupta1@wingify.com>
2019-07-04 13:55:21 +0530

    deleted all files

--- src/Logger/LoggerInterface.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace src\Logger;
-interface LoggerInterface{
-	public function addLog($msg,$level);
-}
--- src/Utils/Constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace src\Utils;
-class Constants{
-    const BASE_URL       =   'https://dev.visualwebsiteoptimizer.com/server-side/';
-    const SETTINGS_URL   =   self::BASE_URL.'settings';
-    const GOAL_URL       =   self::BASE_URL.'track-user';
-    const TRACK_URL      =   self::BASE_URL.'track-user';
-
-}
